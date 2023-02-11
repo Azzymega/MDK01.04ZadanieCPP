c380f1eac1857c3cb9dd46448f763223d2447f8b
Danil <123>
2023-02-11 16:30:33 +0300

    1e - Выполнено

--- ConsoleApplication3.cpp
+++ ConsoleApplication3.cpp
@@ -1,7 +1,4 @@
-﻿// ConsoleApplication3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
 
 
@@ -45,23 +42,17 @@ public:
 
 int main()
 {
-    Printable *print[4];
-    Book book = Book();
-    Magazine magazine = Magazine();
-    print[0] = &book;
-    print[1] = &magazine;
-    print[0]->print();
-    print[1]->print();
-    *print[0] = *print[1];
+    Printable* printables[4];
+    Book firstBook = Book();
+    Magazine firstMagazine = Magazine();
+    Book secondBook = Book();
+    Magazine secondMagazine = Magazine();
+    printables[0] = &firstBook;
+    printables[1] = &firstMagazine;
+    printables[2] = &secondBook;
+    printables[3] = &secondMagazine;
+    for (int i = 0; i < 4; i++)
+    {
+        printables[i]->print();
+    }
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
